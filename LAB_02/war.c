509322d7472d5a225835247e614146eadee638da
jakubkaron <kkaron0313@gmail.com>
2023-03-18 15:52:50 +0100

    finished war.c

--- LAB_02/war.c
+++ LAB_02/war.c
@@ -3,7 +3,15 @@
 #include <limits.h>
 #include <stdbool.h>
 
+#define DECK_SIZE 52
+
 enum state { OK = 0, UNDERFLOW = -1, OVERFLOW = -2 };
+enum result {
+    CONFLICTS_LIMIT_EXCEEDED = 0,
+    WAR_UNRESOLVED = 1,
+    PLAYER_A_WON = 2,
+    PLAYER_B_WON = 3,
+};
 
 int rand_from_interval(int a, int b) {
     if (a > b) return INT_MIN;
@@ -21,7 +29,6 @@ void swap (int array[], int i, int k) {
     array[k] = temp;
 }
 
-// random permutation of integers from [0, n-1]
 void rand_permutation(int n, int array[]) {
     if (n >= 0) {
         for (int i = 0; i < n; i ++) {
@@ -36,292 +43,157 @@ void rand_permutation(int n, int array[]) {
 }
 
 // queue with cyclic buffer
-#define CBUFF_SIZE 52
+#define CBUFF_SIZE DECK_SIZE
+struct Queue {
+    int cbuff[CBUFF_SIZE];
+    int out;
+    int len;
+};
 
-int cbuff_a[CBUFF_SIZE];
-int cbuff_b[CBUFF_SIZE];
-int out_a = 0, out_b = 0, len_a = 0, len_b = 0;
-
-
-int cbuff_push_a(int card_nr) {
-    if (len_a == CBUFF_SIZE) return OVERFLOW;
-    cbuff_a[(out_a + len_a) % (CBUFF_SIZE)] = card_nr;
-    len_a++;
+int cbuff_push(struct Queue* q, int value) {
+    if (q->len == CBUFF_SIZE) return OVERFLOW;
+    q->cbuff[(q->out + q->len) % (CBUFF_SIZE)] = value;
+    q->len++;
     return OK;
 }
 
-int cbuff_push_b(int card_nr) {
-    if (len_b == CBUFF_SIZE) return OVERFLOW;
-    cbuff_b[(out_b + len_b) % (CBUFF_SIZE)] = card_nr;
-    len_b++;
-    return OK;
+int cbuff_pop(struct Queue* q) {
+    int value = q->cbuff[q->out];
+    q->out = (q->out + 1) % (CBUFF_SIZE);
+    q->len--;
+    return value;
 }
 
-int cbuff_pop_a() {
-    if (len_a == 0) return UNDERFLOW;
-    int out_nr = cbuff_a[out_a];
-    out_a = (out_a + 1) % (CBUFF_SIZE);
-    len_a--;
-    return out_nr;
-}
-
-int cbuff_pop_b() {
-    if (len_b == 0) return UNDERFLOW;
-    int out_nr = cbuff_b[out_b];
-    out_b = (out_b + 1) % (CBUFF_SIZE);
-    len_b--;
-    return out_nr;
-}
-
-int cbuff_state_a(void) {
-    return len_a;
-}
-
-int cbuff_state_b(void) {
-    return len_b;
-}
-
-void cbuff_print_a(void) {
-    int i = len_a;
-    int curr_index = out_a;
+void cbuff_print(struct Queue* q) {
+    int i = q->len;
+    int curr_index = q->out;
     while (i) {
-        printf("%d ", cbuff_a[curr_index]);
+        printf("%d ", q->cbuff[curr_index]);
         curr_index = (curr_index + 1) % (CBUFF_SIZE);
         i--;
     }
 }
 
-void cbuff_print_b(void) {
-    int i = len_b;
-    int curr_index = out_b;
-    while (i) {
-        printf("%d ", cbuff_b[curr_index]);
-        curr_index = (curr_index + 1) % (CBUFF_SIZE);
-        i--;
-    }
-}
 
-// war
+struct Player {
+    struct Queue hand;
 
-#define DECK_SIZE 52
+    int current_war_cards[CBUFF_SIZE];
+    int current_card_number;
+    int current_card;
+};
 
-int shuffle_array[DECK_SIZE];
 int war_cards = 0;
-int current_war_cards_a[DECK_SIZE];
-int current_war_cards_b[DECK_SIZE];
-int conflicts = 0;
 
-void shuffle_and_distribute(int main_deck[]) {
+void add_to_hand(struct Player* player, int card_nr){
+    cbuff_push(&player->hand, card_nr);
+}
+
+void hand_to_table(struct Player* player){
+    player->current_card_number = cbuff_pop(&player->hand);
+    player->current_card = player->current_card_number / 4;
+    player->current_war_cards[war_cards] = player->current_card_number;
+}
+
+
+void shuffle_and_distribute(int main_deck[], struct Player* first_player, struct Player* second_player) {
     rand_permutation(DECK_SIZE, main_deck);
 
-    // player A deck
     for (int i = 0; i < DECK_SIZE / 2; i++) {
-        cbuff_push_a(main_deck[i]);
+        add_to_hand(first_player, main_deck[i]);
     }
-    // player B deck
+
     for (int i = DECK_SIZE / 2; i < DECK_SIZE; i++) {
-        cbuff_push_b(main_deck[i]);
+        add_to_hand(second_player, main_deck[i]);
     }
 }
 
-void collect_war_cards_a() {
-    // collect A war cards
+void collect_war_cards(struct Player* winner, struct Player* loser) {
     for (int i = 0; i < war_cards; i++) {
-        cbuff_push_a(current_war_cards_a[i]);
+        add_to_hand(winner,  winner->current_war_cards[i]);
     }
-    // collect B war cards
     for (int i = 0; i < war_cards; i++) {
-        cbuff_push_a(current_war_cards_b[i]);
+        add_to_hand(winner, loser->current_war_cards[i]);
     }
     war_cards = 0;
 }
 
-void collect_war_cards_b() {
-    // collect B war cards
-    for (int i = 0; i < war_cards; i++) {
-        cbuff_push_b(current_war_cards_b[i]);
-    }
+struct Player player_a;
+struct Player player_b;
+int conflicts = 0;
 
-    // collect A war cards
-    for (int i = 0; i < war_cards; i++) {
-        cbuff_push_b(current_war_cards_a[i]);
-    }
-    war_cards = 0;
-}
+int war(int max_conflicts, bool never_war) {
+    bool compare = true;
+    bool war_cards_just_collected = false;
+    while (player_a.hand.len > 0 && player_b.hand.len > 0 && conflicts < max_conflicts) {
+        hand_to_table(&player_a);
+        hand_to_table(&player_b);
+        war_cards++;
 
-int war_standard(int max_conflicts) {
-    int current_a_card;
-    int current_b_card;
-    int current_a_card_nr;
-    int current_b_card_nr;
-    bool was_war;
-
-    while (len_a > 0 && len_b > 0 && conflicts < max_conflicts) {
-        was_war = false;
-        current_a_card_nr = cbuff_a[out_a];
-        current_b_card_nr = cbuff_b[out_b];
-        current_a_card = cbuff_pop_a() / 4;
-        current_b_card = cbuff_pop_b() / 4;
-
-
-        if (current_a_card > current_b_card) {
-            cbuff_push_a(current_a_card_nr);
-            cbuff_push_a(current_b_card_nr);
+        if (compare || never_war) {
             conflicts++;
-        }
-        else if (current_a_card < current_b_card) {
-            cbuff_push_b(current_b_card_nr);
-            cbuff_push_b(current_a_card_nr);
-            conflicts++;
-        }
-        else { // war
-            while (current_a_card == current_b_card && len_a > 0 && len_b > 0 && conflicts < max_conflicts) {
-                if (!was_war) {
-                    conflicts++;
-                    current_war_cards_a[war_cards] = current_a_card_nr;
-                    current_war_cards_b[war_cards] = current_b_card_nr;
-                    war_cards++;
-                }
-                // second card face down
-                current_a_card_nr = cbuff_a[out_a];
-                current_b_card_nr = cbuff_b[out_b];
-                current_a_card = cbuff_pop_a() / 4;
-                current_b_card = cbuff_pop_b() / 4;
-                current_war_cards_a[war_cards] = current_a_card_nr;
-                current_war_cards_b[war_cards] = current_b_card_nr;
-                war_cards++;
-                if (len_a == 0 || len_b == 0) return 2; // b
-                if (conflicts >= max_conflicts) return 1; // a
-                // third card face up
-                conflicts++;
-                current_a_card_nr = cbuff_a[out_a];
-                current_b_card_nr = cbuff_b[out_b];
-                current_a_card = cbuff_pop_a() / 4;
-                current_b_card = cbuff_pop_b() / 4;
-                current_war_cards_a[war_cards] = current_a_card_nr;
-                current_war_cards_b[war_cards] = current_b_card_nr;
-                war_cards++;
-                if (current_a_card > current_b_card) {
-                    collect_war_cards_a();
-                }
-                else if (current_a_card < current_b_card) {
-                    collect_war_cards_b();
-                }
-                was_war = true;
+            if (player_a.current_card > player_b.current_card) {
+                collect_war_cards(&player_a, &player_b);
+                war_cards_just_collected = true;
+            } else if (player_a.current_card < player_b.current_card) {
+                collect_war_cards(&player_b, &player_a);
+                war_cards_just_collected = true;
+            } else if (never_war){
+                add_to_hand(&player_a, player_a.current_card_number);
+                add_to_hand(&player_b, player_b.current_card_number);
+            }else{
+                compare = false;
+                war_cards_just_collected = false;
             }
-            if (len_a == 0 || len_b == 0) return 2; // b
-            if (conflicts >= max_conflicts) return 1; // a
+        } else {
+            compare = true;
+            war_cards_just_collected = false;
         }
     }
+
     if (conflicts >= max_conflicts) {
-        return 1; // a
+        return CONFLICTS_LIMIT_EXCEEDED;
     }
-    else if (len_a == 0 && len_b == 0) {
-        return 2; // b
+    else if ((player_a.hand.len == 0 || player_b.hand.len == 0) && !war_cards_just_collected) {
+        return WAR_UNRESOLVED;
     }
-    else if (len_a != 0 && len_b == 0) {
-        return 3; // c
+    else if (player_a.hand.len != 0 && player_b.hand.len == 0) {
+        return PLAYER_A_WON;
     }
     else {
-        return 4; // d
-    }
-
-}
-
-int war_simplified(int max_conflicts) {
-    int current_a_card;
-    int current_b_card;
-    int current_a_card_nr;
-    int current_b_card_nr;
-    while (len_a > 0 && len_b > 0 && conflicts < max_conflicts) {
-        current_a_card_nr = cbuff_a[out_a];
-        current_b_card_nr = cbuff_b[out_b];
-        current_a_card = cbuff_pop_a() / 4;
-        current_b_card = cbuff_pop_b() / 4;
-        conflicts++;
-        if (current_a_card > current_b_card) {
-            cbuff_push_a(current_a_card_nr);
-            cbuff_push_a(current_b_card_nr);
-        }
-        else if (current_a_card < current_b_card) {
-            cbuff_push_b(current_b_card_nr);
-            cbuff_push_b(current_a_card_nr);
-        }
-        else if (current_a_card == current_b_card) {
-            cbuff_push_a(current_a_card_nr);
-            cbuff_push_b(current_b_card_nr);
-        }
-    }
-    if (conflicts >= max_conflicts) {
-        return 1; // a
-    }
-    else if (len_a == 0 && len_b == 0) {
-        return 2; // b
-    }
-    else if (len_a != 0 && len_b == 0) {
-        return 3; // c
-    }
-    else {
-        return 4; // d
+        return PLAYER_B_WON;
     }
 }
-
 
 
 int main(void) {
     int seed, version, max_conflicts;
-
     scanf("%d %d %d", &seed, &version, &max_conflicts);
-    srand((unsigned) seed); // set the seed
+    srand((unsigned) seed);
 
-    shuffle_and_distribute(shuffle_array);
+    int shuffle_array[DECK_SIZE];
+    shuffle_and_distribute(shuffle_array, &player_a, &player_b);
+    int result = version == 0 ? war(max_conflicts, false) : war(max_conflicts, true);
 
-    // simplified
-    if (version == 1) {
-        switch(war_simplified(max_conflicts)) {
-            case 1:
-                printf("0\n");
-                printf("%d\n", len_a);
-                printf("%d", len_b);
-                break;
-            case 2:
-                printf("1\n");
-                printf("%d\n", len_a);
-                printf("%d\n", len_b);
-                break;
-            case 3:
-                printf("2\n");
-                printf("%d", conflicts);
-                break;
-            case 4:
-                printf("3\n");
-                cbuff_print_b();
-                break;
-        }
-    }
-    // standard
-    else if (version == 0) {
-        switch(war_standard(max_conflicts)) {
-            case 1:
-                printf("0\n");
-                printf("%d\n", len_a + war_cards);
-                printf("%d\n", len_b + war_cards);
-                cbuff_print_b();
-                break;
-            case 2:
-                printf("1\n");
-                printf("%d\n", len_a + war_cards);
-                printf("%d\n", len_b + war_cards);
-                break;
-            case 3:
-                printf("2\n");
-                printf("%d", conflicts);
-                break;
-            case 4:
-                printf("3\n");
-                cbuff_print_b();
-                break;
-        }
+    switch(result) {
+        case CONFLICTS_LIMIT_EXCEEDED:
+            printf("0 ");
+            printf("%d ", player_a.hand.len + war_cards);
+            printf("%d ", player_b.hand.len + war_cards);
+            break;
+        case WAR_UNRESOLVED:
+            printf("1 ");
+            printf("%d ", player_a.hand.len + war_cards);
+            printf("%d ", player_b.hand.len + war_cards);
+            break;
+        case PLAYER_A_WON:
+            printf("2 ");
+            printf("%d", conflicts);
+            break;
+        case PLAYER_B_WON:
+            printf("3\n");
+            cbuff_print(&player_b.hand);
+            break;
     }
 
     return 0;
